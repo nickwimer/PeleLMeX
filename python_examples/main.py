4ccbc0f1b6f72cbd1e3b84f6cf3791e9914b8d60
nickwimer <nicholas.wimer@gmail.com>
2026-05-04 14:54:37 -0700

    testing mpi fix

--- python_examples/main.py
+++ python_examples/main.py
@@ -125,6 +125,9 @@ def main():
         rank_print(f"Initializing AMReX with input file: {input_file}")
         libpelelmex.libpelelmex.initialize_amrex(input_file)
         amrex_initialized = True
+
+        attach_mpi()
+
         rank_print("AMReX initialized.")
 
         rank_print("Initializing SUNDIALS...")
